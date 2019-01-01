f14a36e8e3142622dac968c7c42354d18d154fb1
John Ratcliff <john.w.ratcliff@biola.edu>
2019-01-01 17:40:25 +0000

    Remove some additional ops.  Add global variables for all core calculations.

--- app/components/fire-component.js
+++ app/components/fire-component.js
@@ -1,11 +1,54 @@
 import Component from '@ember/component';
 import { action } from '@ember-decorators/object';
 
-// Controls speed of flames (cannot be greater than 255)
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 300;
+
+// Controls precision of flame calculation (cannot be greater than 255)
 const FIRE = 255;
+// Random seed
+const RANDOM_SEED = 10;
+// How fast flames spread vertically
+const VERTICAL_SPEED = 1.6;
+// How fast flames spread horizontally.  Affects how many gaps are between pixels.  Greater than 1 looks too scattered, lower than .3 looks too linear.
+const HORIZONTAL_SPEED = .7;
+// How fast an individual pixel decays.  This affects the perception of burn speed.
+const DECAY_SPEED = 1.2;
+// How much energy is transferred when a flame grows.  This affects the perception of burn speed.
+const TRANSFER_DECAY = .92;
+
+const halfRandom = RANDOM_SEED / 2;
+
+const redLookup = [];
+const greenLookup = [];
+const blueLookup = [];
+
+// Generate lookup values for colors
+for (let i = 0; i < FIRE - 1; i++) {
+  // Bottom half of flame
+  if (i < FIRE / 2) {
+    redLookup.push(i * 2);
+    greenLookup.push(0);
+
+    // Top half of flame
+  } else {
+    const offset = i - (FIRE * .4);
+
+    redLookup.push(255);
+    greenLookup.push(offset * 1.7);
+  }
+
+  blueLookup.push(0);
+}
+// Last color
+redLookup.push(255);
+greenLookup.push(255);
+blueLookup.push(255);
 
 export default class FireComponent extends Component {
   eternalFlame = true
+  canvasWidth = CANVAS_WIDTH
+  canvasHeight = CANVAS_HEIGHT
 
   didInsertElement() {
     // Generate internal tracking of fire
@@ -74,29 +117,32 @@ export default class FireComponent extends Component {
   // Decay a pixel for this cycle as well as propagate its flame
   _growPixel(y, x) {
     const value = this.pixels[y][x];
-    const ySpread = Math.round(Math.random() * 5);
-    const xSpread = Math.round(ySpread - 2.6);
-    let targetX = x + xSpread;
-    let targetY = y - (ySpread + 1);
+    const random = Math.random() * RANDOM_SEED;
+    const midRandom = random - halfRandom - 1;
 
-    // Lower values should decay faster
-    // This has an interesting relationship with the colors as it helps control the 'glow' from a flame
-    const decay = 8 - (value / 51);
+    let targetX = Math.round(x + (midRandom * HORIZONTAL_SPEED));
+    let targetY = Math.round(y - (random * VERTICAL_SPEED));
+
+    const flameProgress = value / FIRE; // 0 - 1
+    const intensity = 2 - flameProgress; // 1 - 0
+    const decay = Math.round(intensity * random * DECAY_SPEED);
 
     // Check for out of bounds
     if (targetY < 0)
       targetY = 0;
+    if (targetY >= this.canvas.height)
+      targetY = this.canvas.height - 1;
     if (targetX < 0)
       targetX = 0;
-    if (targetX > this.canvas.width)
+    if (targetX >= this.canvas.width)
       targetX = this.canvas.width - 1;
 
     // Energy transfer isn't free!
-    const transferredValue = this.pixels[y][x] - (decay / 4);
+    const transferredValue = this.pixels[y][x] - Math.round(decay * TRANSFER_DECAY);
 
     this.pixels[targetY][targetX] = transferredValue > 0 ? transferredValue : 0;
 
-    // Either we have a temporal flame or we aren't on the bottom y
+    // Should this pixel decay?  That depends on whether this is the bottom pixel and if eternalFlame is true
     if (!this.eternalFlame || this.eternalFlame && y !== this.canvas.height - 1) {
       const updatedValue = value - decay;
 
@@ -120,29 +166,10 @@ export default class FireComponent extends Component {
   _setImagePixel(y, x, value) {
     const index = (y * this.canvas.width * 4) + (x * 4);
 
-    // Only a shade of red
-    if (value < FIRE * .5) {
-      this.image.data[index] = value;   // red
-      this.image.data[index + 1] = 0;       // green
-      this.image.data[index + 2] = 0;       // blue
-
-    // Mix of orange
-    } else if (value < FIRE) {
-      const offset = value - (FIRE * .4);
-
-      this.image.data[index] = 255;         // red
-      this.image.data[index + 1] = offset * 1.7; // green
-      // (value - (FIRE * .4)) * 1.7; // green
-      this.image.data[index + 2] = 0;       // blue
-
-    // White
-    } else if (value === FIRE) {
-      this.image.data[index] = 255;         // red
-      this.image.data[index + 1] = 255;     // green
-      this.image.data[index + 2] = 255;     // blue
-    }
-
-    this.image.data[index + 3] = 255;       // A
+    this.image.data[index + 0] = redLookup[value];       // red
+    this.image.data[index + 1] = greenLookup[value];     // green
+    this.image.data[index + 2] = blueLookup[value];      // blue
+    this.image.data[index + 3] = 255;                    // A
   }
 
   get canvas() {
